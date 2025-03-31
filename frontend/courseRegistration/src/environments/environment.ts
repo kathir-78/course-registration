9d61c35a73fd4f7945518244dc5e888c72ecb6d5
Kathir <Kathir2004official@gmail.com>
2025-03-31 17:30:04 +0530

    Removes unnecessary file

--- frontend/courseRegistration/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,
-    apiUrl : 'http://localhost:3000',
-    CLIENT_ID:'253071569694-tfbr1osfr7joo31k5lslv7kj2m1u8nrq.apps.googleusercontent.com'
-};
